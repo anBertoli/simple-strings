f123122d0bbafd1aa11fda446c61592cd257dad9
Andrea Bertoli <andrea.bertpp@gmail.com>
2021-11-04 19:14:56 +0100

    refactor and split

---
+++ src/string_fmt.h
@@ -0,0 +1,8 @@
+//
+// Created by Andrea Bertoli on 04/11/21.
+//
+
+#ifndef SIMPLE_STRINGS_STRING_FMT_H
+#define SIMPLE_STRINGS_STRING_FMT_H
+
+#endif //SIMPLE_STRINGS_STRING_FMT_H
---
+++ src/string_iter.h
@@ -0,0 +1,8 @@
+//
+// Created by Andrea Bertoli on 04/11/21.
+//
+
+#ifndef SIMPLE_STRINGS_STRING_ITER_H
+#define SIMPLE_STRINGS_STRING_ITER_H
+
+#endif //SIMPLE_STRINGS_STRING_ITER_H
